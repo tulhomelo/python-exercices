ec7d7fac14853f3f34bf674a446e52f9ce931342
Tulho Melo <tulho.oliveiramelo@gmail.com>
2023-04-23 23:23:46 -0300

    ajustes

--- exerc_03.py
+++ exerc_03.py
@@ -6,25 +6,28 @@
 
 def estudo_programacao():
     print("\nResumo dos estudos em programação do mês.")
-    print("\nA seguir você deve informar a quantidade de problemas resolvidos por semana.")
+    print("\nA seguir você deve informar a quantidade de problemas resolvidos por semana:")
+    # Iniciando um dicionário (dict) para guardar as quantidades por semana
+    problemas_resolvidos_semana = {'P1' : None, 'P2' : None, 'P3' : None, 'P4' : None}
+    # Preenchendo o dict com a quantidade de problemas resolvidos
+    for semana in problemas_resolvidos_semana:
+        problemas_resolvidos_semana[semana] = int(input(f"Semana {semana}: "))
+    # Variável para contagem de semanas em que a meta foi cumprida
+    meta_semana = 0
+    # Verificando se a quantidade de problemas resolvidos é igual ou maior que 10
+    for semana, qtd_problemas in problemas_resolvidos_semana.items():
+        if qtd_problemas >= 10:
+            meta_semana += 1
+            print(f"Meta na {semana} foi cumprida!")
 
-# Iniciando um dicionário (dict) para guardar as quantidades por semana
-problemas_resolvidos_semana = {'P1' : None, 'P2' : None, 'P3' : None, 'P4' : None}
-# Preenchendo o dict com a quantidade de problemas resolvidos
-for semana in problemas_resolvidos_semana:
-    problemas_resolvidos_semana[semana] = int(input(f"Informe quantos problemas foram resolvidos na semana {semana}: "))
-# Variável para contagem de semanas em que a meta foi cumprida
-meta_semana = 0
-# Verificando se a quantidade de problemas resolvidos é igual ou maior que 10
-for semana, qtd_problemas in problemas_resolvidos_semana.items():
-    if qtd_problemas >= 10:
-        meta_semana += 1
-        print(f"Meta na {semana} foi cumprida!")
+    # Verificando se a meta foi batida
+    if meta_semana == 4:
+        print("\nParabéns! Meta cumprida em todas as semanas.\n")
+    elif meta_semana > 0:
+        print(f"\nA meta foi cumprida em {meta_semana} semana(s) ao todo.\n")
+    else:
+        print("\nA meta não foi cumprida em nenhuma semana.\n")
 
-# Verificando se a meta foi batida
-if meta_semana == 4:
-    print("Parabéns! Meta cumprida em todas as semanas.")
-elif meta_semana > 0:
-    print("A meta foi cumprida em {semanas_cumpridas} semana(s) ao todo.")
-else:
-    print("A meta não foi cumprida em nenhuma semana.")
+
+# Inicia a função que calcula o programa de estudos semanal
+estudo_programacao()
